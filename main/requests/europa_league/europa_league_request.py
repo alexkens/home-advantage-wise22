e555874239453a651fd7af9b81a80ae50be46294
Roffelcopter451 <kleemannsanchez@campus.tu-berlin.de>
2022-01-17 12:45:19 +0100

    europa_league

---
+++ main/requests/europa_league/europa_league_request.py
@@ -0,0 +1,32 @@
+import json
+import time
+import requests
+
+# ***** Requests API and creates JSON-Files with response. *****
+
+years = list(range(2010, 2020 + 1))
+
+for year in years:
+    # id  muss geändert werden
+    url = f"https://v3.football.api-sports.io/fixtures?league=3&season={year}"
+
+    payload = {}
+    # zugangs login für football api
+    headers = {
+        # api key hier hin nach push und commit wieder mit x'en
+        # fd1348bd73ce135ca127ddbe078ca0b8 XxXxXxXxXxXxXxXxXxXxXxXx
+        'x-rapidapi-key': 'XxXxXxXxXxXxXxXxXxXxXxXx',
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+    # https anfragen protokoll handshake etc.
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # in json umkonvertieren
+    myjson = json.loads(response.text)
+
+    # hier den pfad ändern
+    with open(f'../../resources/europa_league/europa_league_{year}.json', 'w', encoding='utf-8') as outfile:
+        json.dump(myjson, outfile, ensure_ascii=False, indent=4)
+
+    # only 10 requests per minute allowed
+    time.sleep(6)
