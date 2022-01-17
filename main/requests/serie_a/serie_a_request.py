b7cf0b17cb24d5fcb6f485a40bbb79336acd34a2
crazyswayze444 <lilandy@Alexanders-Air.fritz.box>
2022-01-17 12:46:35 +0100

    add serie_a request and recources

---
+++ main/requests/serie_a/serie_a_request.py
@@ -0,0 +1,28 @@
+import json
+import time
+import requests
+
+
+# ***** Requests API and creates JSON-Files with response. *****
+
+years = list(range(2010, 2020 + 1))
+
+for year in years:
+    url = f"https://v3.football.api-sports.io/fixtures?league=135&season={year}"
+
+    payload = {}
+    # XxXxXxXxXxXxXxXxXxXxXxXx
+    headers = {
+        'x-rapidapi-key': 'fd1348bd73ce135ca127ddbe078ca0b8',
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    myjson = json.loads(response.text)
+
+    with open(f'../../resources/serie_a/serie_a_{year}.json', 'w', encoding='utf-8') as outfile:
+        json.dump(myjson, outfile, ensure_ascii=False, indent=4)
+
+    # only 10 requests per minute allowed
+    time.sleep(6)
