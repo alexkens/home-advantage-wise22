546ed99cf63ea3d6e432942262442b129114a834
fsix <six@campus.tu-berlin.de>
2022-01-19 10:37:25 +0100

    method fix

--- main/analysis/AnalysisUtil.py
+++ main/analysis/AnalysisUtil.py
@@ -6,52 +6,10 @@ import pandas as pd
 
 class AnalysisHelper:
 
-    def get_fixtures(self, league, year):
-        if not isinstance(league, list):
-            return self.__get_fixtures_of_league(self, league, year)
-        else:
-            return self.__get_fixtures_of_more_leagues(self, league, year)
+    def get_fixtures(self, leagues, years):
+        dfs = []
 
-    @staticmethod
-    def __get_fixtures_of_league(self, league, years):
-        if not isinstance(years, list):
-            with open(f"../resources/{league}/{league}_{years}.json", "r") as read_file:
-                data = json.load(read_file)
-                df = pd.json_normalize(data['response'][0])
-                for response in data['response'][1:]:
-                    df1 = pd.json_normalize(response)
-                    df = df.append(df1, ignore_index=True)
-
-            return df
-        else:
-            dfs = []
-            for year in years:
-                with open(f"../resources/{league}/{league}_{year}.json", "r") as read_file:
-                    data = json.load(read_file)
-                    df = pd.json_normalize(data['response'][0])
-                    for response in data['response'][1:]:
-                        df1 = pd.json_normalize(response)
-                        df = df.append(df1, ignore_index=True)
-                dfs.append(df)
-
-            return ft.reduce(self.__append, dfs).reset_index()
-
-    @staticmethod
-    def __get_fixtures_of_more_leagues(self, leagues, years):
-        if not isinstance(years, list):
-            dfs = []
-            for league in leagues:
-                with open(f"../resources/{league}/{league}_{years}.json", "r") as read_file:
-                    data = json.load(read_file)
-                    df = pd.json_normalize(data['response'][0])
-                    for response in data['response']:
-                        df1 = pd.json_normalize(response)
-                        df = df.append(df1, ignore_index=True)
-                dfs.append(df)
-
-            return ft.reduce(self.__append, dfs).reset_index()
-        else:
-            dfs = []
+        if (not isinstance(leagues, str)) | (not isinstance(years, str)):
             for league in leagues:
                 for year in years:
                     with open(f"../resources/{league}/{league}_{year}.json", "r") as read_file:
@@ -61,9 +19,10 @@ class AnalysisHelper:
                             df1 = pd.json_normalize(response)
                             df = df.append(df1, ignore_index=True)
                     dfs.append(df)
-                dfs.append(df)
 
             return ft.reduce(self.__append, dfs).reset_index()
+        else:
+            print("Please pass parameters as lists!")
 
     @staticmethod
     def __append(a, b):
