f6ec343f6836fade52127072e9fac3f630e86547
fsix <six@campus.tu-berlin.de>
2022-02-08 20:42:49 +0100

    league ids as enums

---
+++ main/analysis/all_enums.py
@@ -0,0 +1,11 @@
+from enum import IntEnum
+
+
+class LeagueID(IntEnum):
+    BUNDESLIGA_ID = 78
+    CHAMPIONS_LEAGUE_ID = 2
+    EUROPA_LEAGUE_ID = 3
+    LA_LIGA_ID = 140
+    LIGUE_1_ID = 61
+    PREMIER_LEAGUE_ID = 39
+    SERIE_A_ID = 135
